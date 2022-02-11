797685614ce5d0e40f2774c301296e7470369806
strick-j <skazoo@gmail.com>
2022-02-11 16:50:01 -0500

    Working aws-sig-v4 signer for Conjur

--- go.mod
+++ go.mod
@@ -4,12 +4,20 @@ go 1.17
 
 require (
 	github.com/aws/aws-sdk-go-v2 v1.13.0
+	github.com/aws/aws-sdk-go-v2/config v1.13.1
 	github.com/aws/aws-sdk-go-v2/credentials v1.8.0
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.10.0
+	github.com/codingconcepts/env v0.0.0-20200821220118-a8fbf8d84482
 	github.com/sirupsen/logrus v1.8.1
 )
 
 require (
-	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.10.0 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.4 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.2.0 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.7.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.9.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.14.0 // indirect
 	github.com/aws/smithy-go v1.10.0 // indirect
 	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
 )
--- main.go
+++ main.go
@@ -6,28 +6,38 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"net/http/httputil"
 	"net/url"
+	"reflect"
 	"time"
 
 	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
+	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials/ec2rolecreds"
+	"github.com/codingconcepts/env"
 	log "github.com/sirupsen/logrus"
 )
 
 type SigHeader struct {
-	Authorization     string `json:"Authorization"`
-	Signature         string `json:"Signature"`
+	Host              string `json:"host"`
 	XAmzDate          string `json:"x-amz-date"`
 	XAmzSecurityToken string `json:"x-amz-security-token"`
-	Host              string `json:"host"`
+	XAmzContentSHA256 string `json:"x-amz-content-sha256"`
+	Authorization     string `json:"authorization"`
 }
 
-type Conjur struct {
-	url    string // Conjur Host e.g. https://conjur.yourdomain.com
-	acct   string // Conjur Account e.g. default
-	secret string // Conjur Secret e.g. policy/path/variable-id
-	host   string // Host to Authenticate as e.g. host/policy/prefix/id
+type ConjurDetails struct {
+	Url       string `env:"CONJUR_APPLIANCE_URL" required="true"` // Conjur Host e.g. https://conjur.yourdomain.com
+	Acct      string `env:"CONJUR_ACCOUNT" required="true"`       // Conjur Account e.g. default
+	HostId    string `env:"CONJUR_AUTHN_LOGIN" required="true"`   // Host to Authenticate as e.g. host/policy/prefix/id
+	ServiceId string `env:"AUTHN_IAM_SERVICE_ID" required="true"` // Authentication Service e.g. prod
 }
+
+var (
+	xAmzContentSHA256 string = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+	serviceID         string = "sts"       // Used in signer function
+	region            string = "us-east-1" // Region must be us-east-1 for the IAM Service Call
+)
 
 func init() {
 	// Log as JSON instead of the default ASCII formatter.
@@ -40,72 +50,109 @@ func init() {
 }
 
 func main() {
-
-	// Returns initialized Provider using EC2 Instance Metadata details
-	svc := ec2rolecreds.New()
+	// Returns initialized Provider using EC2 IMDS Client by default
+	svc := ec2rolecreds.New(func(options *ec2rolecreds.Options) {
+		expTime := time.Now().UTC().Add(1 * time.Hour).Format("2006-01-02T15:04:05Z")
+		config.WithRegion(region)
+		log.WithFields(log.Fields{"Category": "Credentials"}).Debug("Credential Expiration: ", expTime)
+	})
 
 	// Retrieve retrieves credentials from the EC2 service.
 	creds, err := svc.Retrieve(context.Background())
 	if err != nil {
-		log.WithFields(log.Fields{"event": err}).Fatal("Unable to retrieve credentials from EC2 Service: ")
+		log.WithFields(log.Fields{"event": err}).Fatal("Unable to retrieve credentials from EC2 Service")
 	}
+	log.WithFields(log.Fields{"Category": "Credentials"}).Debug("AccessKeyID: ", creds.AccessKeyID)
+	log.WithFields(log.Fields{"Category": "Credentials"}).Debug("SecretKeyID: ", creds.SecretAccessKey)
+	log.WithFields(log.Fields{"Category": "Credentials"}).Debug("Session Token: ", creds.SessionToken)
 
-	// Create request
-	host := "sts.amazonaws.com/"
-	req, err := http.NewRequest("GET", host, nil)
+	// Create STS Request URL
+	stsUrl := &url.URL{
+		Scheme:   "https",
+		Host:     "sts.amazonaws.com",
+		Path:     "/",
+		RawQuery: "Action=GetCallerIdentity&Version=2011-06-15",
+	}
+	stsReq, err := http.NewRequest("GET", stsUrl.String(), nil)
 	if err != nil {
 		log.WithFields(log.Fields{"event": err}).Fatal("Expected no error from request")
 	}
-	req.URL.Path = `/?Action=GetCallerIdentity&Version=2011-06-15`
 
-	// Create Signer
+	// Create Signer using aws-sdk-go-v2/aws/signer
 	signer := v4.NewSigner()
-	err = signer.SignHTTP(context.Background(), creds, req, `e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855`, "sts", "us-west-2", time.Unix(0, 0))
+	err = signer.SignHTTP(context.Background(), creds, stsReq, xAmzContentSHA256, serviceID, region, time.Now().UTC(), func(o *v4.SignerOptions) {
+		o.DisableURIPathEscaping = false
+		o.LogSigning = true
+	})
 	if err != nil {
 		log.WithFields(log.Fields{"event": err}).Fatal("Expected no error from request")
 	}
+
+	// Save a copy of this request for debugging.
+	stsRequestDump, err := httputil.DumpRequestOut(stsReq, true)
+	if err != nil {
+		log.WithFields(log.Fields{"event": err}).Fatal("Error dumping Conjur http.request")
+	}
+	log.WithFields(log.Fields{"category": "http.request"}).Debug("STS HTTP request: ", string(stsRequestDump))
 
 	// Create JSON from signer response header
-	sigHeader := &SigHeader{Authorization: req.Header.Get("Authorization"), XAmzDate: req.Header.Get("X-Amz-Date"), XAmzSecurityToken: req.Header.Get("X-Amz-Security-Token"), Host: host}
-	headerJson, err := json.Marshal(sigHeader)
+	sigHeader := SigHeader{
+		Host:              stsUrl.Host,
+		XAmzDate:          stsReq.Header.Get("X-Amz-Date"),
+		XAmzSecurityToken: stsReq.Header.Get("X-Amz-Security-Token"),
+		Authorization:     stsReq.Header.Get("Authorization"),
+	}
+	payload, err := json.Marshal(sigHeader)
 	if err != nil {
 		log.WithFields(log.Fields{"event": err}).Fatal("Unable to create JSON Header for Conjur Auth Request")
-	} else {
-		log.WithFields(log.Fields{"event": string(headerJson)}).Info("Created JSON Header for Conjur Auth Request")
 	}
+	log.WithFields(log.Fields{"category": "header"}).Debug("Json Header from Struct: ", string(payload))
 
 	// Declare / Read in Conjur Information
-	conjur := Conjur{
-		url:    "https://<conjur host placeholder>",
-		acct:   "<conjur account placeholder>",
-		secret: "<secret placehoser>",
-		host:   "<host placehoder>",
+	conjur := ConjurDetails{}
+	if err := env.Set(&conjur); err != nil {
+		log.WithFields(log.Fields{"Environment Variable": err}).Fatal("Required Environment Variable not set")
 	}
 
-	// Build Conjur Auth URL
-	// Authorization URL = "Conjur Host Name" + "Authenticator" + "Account" + "Escaped Host Name" + "/authenticate"
-	authUrl := conjur.url + "/authn-iam/prod/" + conjur.acct + "/" + url.QueryEscape(conjur.host) + "/authenticate"
+	// Ensure Conjur Details are available and the fields aren't empty
+	v := reflect.ValueOf(conjur)
+	for i := 0; i < v.NumField(); i++ {
+		if v.Field(i).Interface() == "" {
+			log.Info("Conjur Environment Variable not set: ", v.Field(i).Interface())
+		}
+	}
 
+	// Build Conjur URL (Path Escape required on HOST ID to convert / to %2F)
+	authUrl := conjur.Url + "/authn-iam/" + conjur.ServiceId + "/" + conjur.Acct + "/" + url.PathEscape(conjur.HostId) + "/authenticate"
+	log.WithFields(log.Fields{"category": "url"}).Debug("Conjur Auth URL: ", authUrl)
+
+	// Generate Conjur Client
 	client := &http.Client{}
-	conjurReq, _ := http.NewRequest(http.MethodPost, authUrl, bytes.NewBuffer(headerJson))
+	conjurReq, err := http.NewRequest("POST", authUrl, bytes.NewBuffer(payload))
+	if err != nil {
+		log.WithFields(log.Fields{"event": err}).Fatal("Expected no error from request")
+	}
+	conjurReq.Header.Add("Content-Type", "text/plain")
+	conjurReq.Header.Add("Accept", "*/*")
+
+	// Save a copy of this request for debugging.
+	requestDump, err := httputil.DumpRequestOut(conjurReq, true)
+	if err != nil {
+		log.WithFields(log.Fields{"event": err}).Fatal("Error dumping Conjur http.request")
+	}
+	log.WithFields(log.Fields{"category": "http.request"}).Debug("Conjur HTTP request: ", string(requestDump))
 
 	resp, err := client.Do(conjurReq)
 	if err != nil {
 		log.WithFields(log.Fields{"event": err}).Fatal("No response from Conjur Host")
+	} else if resp.StatusCode == 401 || resp.StatusCode == 404 {
+		log.WithFields(log.Fields{"Status Code": resp.StatusCode}).Fatal(resp.Status)
 	}
-	if resp.StatusCode == 401 {
-		log.WithFields(log.Fields{"Status Code": resp.StatusCode}).Fatal(ioutil.ReadAll(resp.Body))
-	}
-
 	defer resp.Body.Close()
-
-	log.Info("Conjur Auth Response status code: ", resp.StatusCode)
 
 	respBytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Info("No response recieved from host: ", err)
+		log.WithFields(log.Fields{"event": err}).Fatal("Response Body Empty")
 	}
-	log.Info("Byte Response: ", string(respBytes))
-
-	// TODO Build secret retrieval with Auth Token
+	log.WithFields(log.Fields{"Response": "Success"}).Info("Host Response: ", string(respBytes))
 }
